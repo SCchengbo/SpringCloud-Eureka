01e1290f6dee1221f95798123f7557cecade862a
Administrator <Administrator@NNH1J2ASSQP10BA>
2019-05-10 15:14:20 +0800

    SpirngCloud-eureka注册服务中心

--- src/main/java/com/EurekaApp.java
+++ src/main/java/com/EurekaApp.java
@@ -3,12 +3,13 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+//注册服务中心搭建
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApp {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(EurekaApp.class, args);
 	}
 
